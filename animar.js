ac567b846cf98a080488fed31896a44df5e8f52a
Hudson Castro <hudsoncastro24@gmail.com>
2024-11-09 18:36:39 -0300

    àdcionando botão

--- animar.js
+++ animar.js
@@ -1,15 +1,18 @@
-function animar(){
-    const botao = document.getElementById('botao')
-    botao.classList.toggle('ativar')
-}
 
 const menuDiv = document.getElementById('menu-mobile')
-const botaAnimar = document.getElementById('botao')
+const botaoAnimar = document.getElementById('botao')
 
 menuDiv.addEventListener('click', animar )
 
 function animar(){
     menuDiv.classList.toggle('abrir')
-    botaAnimar.classList.toggle('ativo')
+    botaoAnimar.classList.toggle('ligar')
 
 }
+
+
+
+
+
+
+
